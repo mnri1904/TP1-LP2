1e9d8be6ceb9145c27e5fc4cde4e7fb05cdefdea
duw4ng <alexavalos330@gmail.com>
2026-04-15 10:26:37 -0300

    Enhance comments in gameconfig.h
    Added comments to the GameSettings structure and its global variable for better clarity.

--- version gráfica/src/gameconfig.h
+++ version gráfica/src/gameconfig.h
@@ -3,14 +3,24 @@
 
 #include <QString>
 
+/*
+ * Estructura GameSettings
+ *
+ * Guarda todas las configuraciones del juego seleccionadas por el usuario.
+ * Se utiliza como configuración global accesible desde distintas partes del programa (UI, lógica, renderizado).
+ */
 struct GameSettings {
-    int filas = 3;
-    int columnas = 3;
+    int filas = 3; //cantidad de filas
+    int columnas = 3; //cantidad de columnas
     int carnivoroVariante = 0;   // 0 = Warwick, 1 = Naafiri
     int herbivoroVariante = 0; // 0 = Teemo, 1 = Fizz
     int plantaVariante = 0; // 0 Honeyfruit, 1 Bloodroses
 };
-
+/*
+ * Variable global de configuración.
+ *
+ * Se declara como extern para poder ser utilizada en múltiples archivos.
+ */
 extern GameSettings gSettings;
 
 #endif
