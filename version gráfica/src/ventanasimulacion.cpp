6c002c4add2da52ba075794f6581a9df93fd52b7
duw4ng <alexavalos330@gmail.com>
2026-04-14 22:35:03 -0300

    Update ventanasimulacion.cpp

--- version gráfica/src/ventanasimulacion.cpp
+++ version gráfica/src/ventanasimulacion.cpp
@@ -54,63 +54,6 @@ void VentanaSimulacion::aplicarConfiguracionActual()
     configurarDesdeEcosistema();
 }
 
-/*
-void VentanaSimulacion::construirTableroVisual()
-{
-    vaciarLayout(layoutTablero);
-
-    layoutTablero->setSpacing(0);
-    layoutTablero->setContentsMargins(0,0,0,0);
-
-    //labelsTablero.resize(filas);
-    labelsTablero.assign(filas, std::vector<QLabel*>(columnas, nullptr));
-
-    for (int i = 0; i < filas; ++i) {
-        for (int j = 0; j < columnas; ++j) {
-            auto *label = new QLabel();
-            label->setFixedSize(48, 48);
-            label->setAlignment(Qt::AlignCenter);
-            label->setScaledContents(true);
-            label->setStyleSheet(R"(
-                border: none;
-                margin: 0px;
-                padding: 0px;
-            )");
-
-            layoutTablero->addWidget(label, i, j);
-            labelsTablero[i][j] = label;
-        }
-    }
-}
-*/
-/*
-void VentanaSimulacion::actualizarVista()
-{
-    const auto &tablero = eco->getTablero();
-
-    for(int i = 0; i < filas; i++) {
-        for(int j = 0; j < columnas; j++) {
-
-            QLabel *lbl = labelsTablero[i][j];
-            char c = tablero[i][j];
-
-            if(c == 'P') {
-                lbl->setPixmap(iconoPlanta.scaled(40,40,Qt::KeepAspectRatio,Qt::SmoothTransformation));
-            }
-            else if(c == 'H') {
-                lbl->setPixmap(iconoHerbivoro.scaled(40,40,Qt::KeepAspectRatio,Qt::SmoothTransformation));
-            }
-            else if(c == 'C') {
-                lbl->setPixmap(iconoCarnivoro.scaled(40,40,Qt::KeepAspectRatio,Qt::SmoothTransformation));
-            }
-            else {
-                lbl->clear();
-            }
-        }
-    }
-}
-*/
-
 void VentanaSimulacion::actualizarContadores()
 {
     int P = 0, H = 0, C = 0;
